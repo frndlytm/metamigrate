320bb487c7225b0a01b29c5e089a481f4d632a56
Christian DiMare <frndlytm@gmail.com>
2018-05-09 11:29:24 -0400

    Moved the Meta class from the migrate script to manage the connection information.

--- manager.py
+++ manager.py
@@ -8,3 +8,56 @@
     This is effectively a server that serves up table creation statements from
     some metadata.
 """
+import pandas as pd
+from urllib.parse import quote_plus
+from sqlalchemy import create_engine
+from table import TableFactory
+
+
+class MetaManager:
+    """
+    A container for our connection information and for
+    selecting all data from a table.
+    """
+    def __init__(self, server, database, driver, port, env, flavor):
+        """
+        Establish the connection data.
+        """
+        self.flavor = flavor
+        self.server = server
+        self.database = database
+        self.driver = driver
+        self.port = port
+        self.factory = TableFactory(env, flavor)
+
+
+    def __enter__(self):
+        """
+        Connect to the database for querying.
+        """
+
+        self.conn = create_engine(str(self))
+        return self
+
+    def __exit__(self, *args):
+        """
+        Disconnect on wrap-up.
+        """
+        self.conn.close()
+
+    def __str__(self):
+        return '{}+pyodbc:///?odbc_connect={}'.format(self.flavor, self._params())
+
+    def _params(self):
+        return quote_plus(
+            'DRIVER={};PORT={};SERVER={};DATABASE={};Trusted_Connection=yes;'
+            .format(self.driver, self.port, self.server, self.database)
+        )
+
+    def select_all(self, table):
+        """
+        Select all data from a table on the connection.
+        """
+        query = "select * from {0}".format(table)
+        data = pd.read_sql(query, self.conn)
+        return data
