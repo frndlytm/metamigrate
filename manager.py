8d10591a16b7abcd9fc38bb70781eba1d537a438
Christian DiMare <frndlytm@gmail.com>
2018-05-09 10:46:29 -0400

    Create a placeholder for a server-like object to produce table reprs.

---
+++ manager.py
@@ -0,0 +1,10 @@
+"""
+@author: frndlytm
+@name: metamigrate.manager
+@description:
+    Manages the connection properties, templating environment, and factories
+    that all communicate.
+
+    This is effectively a server that serves up table creation statements from
+    some metadata.
+"""
