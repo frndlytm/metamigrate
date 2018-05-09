19ec855ce6e2edff0d993e9b87713620a7882630
Christian DiMare <frndlytm@gmail.com>
2018-05-09 11:45:53 -0400

    Updated manager for minor bugs and documentation.

--- manager.py
+++ manager.py
@@ -15,10 +15,10 @@ from table import TableFactory
 
 
 class MetaManager:
-    """
-    A container for our connection information and for
-    selecting all data from a table.
-    """
+    #
+    # A container for our connection information and for
+    # selecting all data from a table.
+    #
     def __init__(self, flavor, server, database, driver, port, env):
         """
         Establish the connection data.
@@ -28,35 +28,56 @@ class MetaManager:
         self.database = database
         self.driver = driver
         self.port = port
+        self._connection = None
         self.factory = TableFactory(env, flavor)
 
+    #
+    # connection is opened like a context manager through
+    # __enter__ and closed through __exit__.
+    #
+    @property
+    def connection(self):
+        return self._connection
+    @connection.setter
+    def connection(self, conn):
+        self._connection = conn
 
+
+    #
+    # enable context manager syntex for the MetaManager connection.
+    #
     def __enter__(self):
-        """
-        Connect to the database for querying.
-        """
-        self.conn = create_engine(str(self))
+        self.connection = create_engine(str(self))
         return self
 
     def __exit__(self, *args):
-        """
-        Disconnect on wrap-up.
-        """
-        self.conn.dispose()
+        self.connection.dispose()
 
+
+    #
+    # serialize the MetaManager as a connection string. Enables using
+    # str(self) in the __enter__ method.
+    #
     def __str__(self):
         return '{}+pyodbc:///?odbc_connect={}'.format(self.flavor, self._params())
 
+    #
+    # helper for __str__ to simplify the connection string.
+    #
     def _params(self):
         return quote_plus(
             'DRIVER={};PORT={};SERVER={};DATABASE={};Trusted_Connection=yes;'
             .format(self.driver, self.port, self.server, self.database)
         )
 
+    #
+    # standard SQL operation on a database. Might want to revise this to
+    # be more restrictive on a per-project schema.
+    #
     def select_all(self, table):
         """
         Select all data from a table on the connection.
         """
         query = "select * from {0}".format(table)
-        data = pd.read_sql(query, self.conn)
+        data = pd.read_sql(query, self.connection)
         return data
