5d9117e28cc320d68646f6861f7ddb59938c6809
Christian DiMare <frndlytm@gmail.com>
2018-05-09 12:29:54 -0400

    Updated column factory to validate nullable.

--- column.py
+++ column.py
@@ -5,9 +5,22 @@
     Manages the attributes of a column in a table. Since the table manages the template,
     we only care about encoding the metadata consistently.
 """
+class ColumnFactory:
+    def make_column(self, position, name, columntype, nullable=1, default=None):
+        return Column(
+            position, name, columntype, self.convert_nullable(nullable), default
+        )
+
+    def convert_nullable(self, nullable):
+        if nullable in [0, 1]: return nullable
+        elif nullable == 'YES': return 1
+        elif nullable == 'NO': return 0
+        else: raise ValueError
+
+
 
 class Column:
-    def __init__(self, position, name, columntype, nullable=1, default=None):
+    def __init__(self, position, name, columntype, nullable, default):
         self._position = position
         self.name = name
         self.columntype = columntype
