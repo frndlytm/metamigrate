3b257a605dabef5333f2541e63129b82c2c229ee
Christian DiMare <frndlytm@gmail.com>
2018-05-25 09:01:13 -0400

    Rebranded columns to fields, dropped the Field object. Updated the FieldFactory to produce dicts and do house any pre-processing on the data. Not sure about the entry point for a FieldFactory yet though...

---
+++ field.py
@@ -0,0 +1,24 @@
+"""
+@author: frndlytm
+@name: metamigrate.column
+@description:
+    Manages the attributes of a column in a table. Since the table manages the template,
+    we only care about encoding the metadata consistently.
+"""
+class FieldFactory:
+    def make(self, position, name, data_type, is_nullable=1, default=None):
+        return dict(
+            position=position,
+            name=name,
+            data_type=data_type,
+            is_nullable=self.convert_nullable(is_nullable),
+            default=default
+        )
+
+    def convert_nullable(self, nullable):
+        if nullable in [False, True]: return nullable
+        elif nullable == 'YES' or nullable == 1: return True
+        elif nullable == 'NO' or nullable == 0: return False
+        else: raise ValueError
+
+
