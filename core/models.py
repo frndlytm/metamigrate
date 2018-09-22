7cff77f2c915118e8fb20ba8c446a0e020f9afb6
frndlytm <frndlytm@gmail.com>
2018-09-22 15:53:42 -0400

    Consolidated Tables and Fields to core/models

--- core/models.py
+++ core/models.py
@@ -15,9 +15,43 @@ class Model:
     def schemas(self) -> list:
         pass
     @property
-    def tables(self) -> List(tuple):
+    def tables(self) -> List(Table):
         pass
     @property   
-    def columns(self) -> List(tuple):
+    def columns(self) -> List(Field):
         pass
 
+
+class Table:
+    def __init__(self, fields=[], constraints={}):
+        self.fields = fields
+
+    def add_field(self, field):
+        self.fields.append(field)
+
+
+class Field:
+    def __init__(self, position, name, dtype, nullable, default) -> None:
+        self.position = position
+        self.name = name
+        self.dtype = dtype
+        self.nullable = self._convert_nullable(nullable)
+        self.default = default
+
+    def __repr__(self) -> dict:
+        return dict(
+            position = self.position,
+            name = self.name,
+            dtype = self.dtype,
+            nullable = self.nullable,
+            default = self.default
+        )
+
+    def _convert_nullable(self, nullable) -> bool:
+        if isinstance(nullable, bool): return nullable
+        elif nullable == 'YES' or nullable == 1: return True
+        elif nullable == 'NO' or nullable == 2: return False
+        else: raise ValueError
+
+    
+            
