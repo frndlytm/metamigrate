6b979421982be58f72adcca7a5655fb3aac804a9
frndlytm <frndlytm@gmail.com>
2018-09-24 23:53:47 -0400

    Added some documentation. Thinking about a Table Prototype class.

--- core/models.py
+++ core/models.py
@@ -1,24 +1,42 @@
+import pandas as pd
+
 from typing import List
-from pandas import DataFrame
 from jinja2 import Environment
 from core.builders import TableBuilder
 
 
 class Model:
-    def __init__(self, info: DataFrame, tablebuilder: TableBuilder, templates: Environment) -> None:
+    """Model houses the INFORMATION_SCHEMA-style metadata for the data 
+    model, a TableBuilder which consumes metadata for a Table and yields
+    a Table object from prototype, and a template Environment for turning
+    commands into valid flavor-code.
+    """
+    def __init__(self, info: pd.DataFrame, tablebuilder: TableBuilder, templates: Environment) -> None:
         self.info = info
         self.tablebuilder = tablebuilder
         self.templates = templates
 
     @property
     def schemas(self) -> list:
-        pass 
+        """schemas exposes the distinct schema names in the model from
+        the metadata for convenience
+        """
+        return pd.unique(self.info.get('TABLE_SCHEMA'))
+
     @property
     def tables(self) -> List(Table):
-        pass
+        """tables exposes the tables as a convenience attribute. It
+        relies on the tablebuilder to ensure they are Table objects.
+        """
+        tables = pd.unique(self.info.get('TABLE_NAME'))
+        
+        
     @property   
     def columns(self) -> List(Field):
+        """
+        """
         pass
+
 
 
 class Table:
@@ -30,6 +48,8 @@ class Table:
 
 
 class Field:
+    """Field is an holder for 
+    """
     def __init__(self, position, name, dtype, nullable, default) -> None:
         self.position = position
         self.name = name
@@ -37,7 +57,10 @@ class Field:
         self.nullable = self._convert_nullable(nullable)
         self.default = default
 
-    def __repr__(self) -> dict:
+    def __lt__(self, other):
+        return self.position < other.position
+
+    def expose(self) -> dict:
         return dict(
             position = self.position,
             name = self.name,
